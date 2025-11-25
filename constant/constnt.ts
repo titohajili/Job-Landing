ae4bad62af32f2ec5fb0bc0f20858d0d5d23c8bc
titohajili <maqatoto963@gmail.com>
2025-11-25 15:26:52 +0400

    Navbar

---
+++ constant/constnt.ts
@@ -0,0 +1,32 @@
+export const NavLinks = [
+    {
+        id:1,
+        url: '#',
+        label: 'Home'
+    },
+    {
+        id:2,
+        url: '#',
+        label: 'Find Job'
+    },
+    {
+        id:3,
+        url: '#',
+        label: 'Candidates'
+    },
+    {
+        id:4,
+        url: '#',
+        label: 'Employers'
+    },
+    {
+        id:5,
+        url: '#',
+        label: 'Blog'
+    },
+    {
+        id:6,
+        url: '#',
+        label: 'Contact'
+    }
+]
