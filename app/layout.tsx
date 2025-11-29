5d222dc194572c439c189aee5ee519c52370d9f8
titohajili <maqatoto963@gmail.com>
2025-11-29 17:49:01 +0400

    Dark Mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
+import Provider from "@/components/Hoc/Provider";
 
 const font = Roboto({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -21,12 +22,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${font.className} antialiased`}
       >
-        <ResponsiveNav/>
-        {children}
+        <Provider>
+
+          <ResponsiveNav/>
+          {children}
+
+        </Provider>
       </body>
     </html>
   );
--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,4 +1,5 @@
 'use client';
+import ThemeToggler from '@/components/Helper/ThemeToggler';
 import { NavLinks } from '@/constant/constnt';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
@@ -29,16 +30,16 @@ const Nav = ({openNav}:Props) => {
   },[])
 
   return (
-    <div className={`transition-all duration-200 h-[12vh] z-1000 fixed w-full ${navBg ? 'bg-white shadow-md' : "fixed"}`}>
+    <div className={`transition-all duration-200 h-[12vh] z-1000 fixed w-full ${navBg ? 'bg-white dark:bg-gray-900 shadow-md' : "fixed"} `}>
       <div className='flex items-center h-full justify-between w-[92%] mx-auto'>
         <div className='flex items-center sm:space-x-20 '>
           {/* LOGO */}
 
           <div className='flex items-center space-x-2'>
-            <div className='w-10 h-10 bg-cyan-800 rounded-full flex items-center justify-center flex-col'>
-              <LuNetwork className='w-5 h-5  text-white'/>
+            <div className='w-10 h-10 bg-cyan-800 dark:bg-white rounded-full flex items-center justify-center flex-col'>
+              <LuNetwork className='w-5 h-5  text-white dark:text-black'/>
             </div>
-            <h1 className='text-cl hidden sm:block md:text-2xl text-cyan-800 font-bold'>DevHire</h1>
+            <h1 className='text-cl hidden sm:block md:text-2xl text-cyan-800 dark:text-white font-bold'>DevHire</h1>
           </div>
 
           {/* NAV LINKS */}
@@ -46,7 +47,7 @@ const Nav = ({openNav}:Props) => {
           <div className='hidden lg:flex items-center space-x-10'>
             {
               NavLinks.map((link)=>{
-                return <Link key={link.id} href={link.url} className='text-base hover:text-cyan-700 font-medium transition-all duration-200'>
+                return <Link  key={link.id} href={link.url} className='text-base hover:text-cyan-700 dark:hover:text-cyan-200 font-medium transition-all duration-200'>
                   <p>{link.label}</p>
                 </Link>
               })
@@ -58,12 +59,13 @@ const Nav = ({openNav}:Props) => {
         <div className='flex items-center space-x-4'>
 
           {/* Login/register button */}
-          <button className='px-8 py-2.5 text-xs sm:text-sm rounded-lg cursor-pointer bg-gray-700 hover:bg-gray-300 transition-all duration-300'>Login / Register</button>
+          <button className='px-8 py-2.5 text-xs sm:text-sm rounded-lg cursor-pointer bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-900 hover:bg-gray-300 transition-all duration-300'>Login / Register</button>
 
           {/* Job post button */}
           <button className='px-8 py-2.5 text-sm hidden sm:block cursor-pointer rounded-lg bg-cyan-700 hover:bg-cyan-900 transition-all duration-300 text-white'>Job Post</button>
 
           {/* Theme Toggler */}
+          <ThemeToggler/>
           
           {/* Burger menu */}
           <HiBars3BottomRight onClick={openNav} className='w-8 h-8 cursor-pointer text-black lg:hidden'/>
