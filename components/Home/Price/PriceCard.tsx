0f386689f1075cf3b0aacd4d6d398121d2800699
titohajili <maqatoto963@gmail.com>
2025-12-05 18:54:15 +0400

    Price Card

--- components/Home/Price/PriceCard.tsx
+++ components/Home/Price/PriceCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BiCheck } from 'react-icons/bi';
 
 type Props = {
     type: string;
@@ -14,7 +15,38 @@ const PriceCard = ({price,type}:Props) => {
             Recomended
         </div>}
 
+        {/* Type  */}
+    <h1 className='mt-6 text-xl font-bold text-blue-900 dark:text-blue-200'>{type}</h1>
+        {/* Price */}
+    <div className='mt-3'>
+        <span className='text-4xl font-bold text-bl950 dark:text-white'>${price}</span> {""}
+        / Monthly
+    </div>
 
+        {/* feature list */}
+        <div className='mt-12'>
+            <div className='flex items-center mv-6 space-x-1'>
+                <BiCheck  className='w-7 h-7 '/>
+                <span className='text-gray-700 dark:text-gray-300'>1 Job Posting</span>
+            </div>
+            <div className='flex items-center mv-6 space-x-1'>
+                <BiCheck  className='w-7 h-7 '/>
+                <span className='text-gray-700 dark:text-gray-300'>0 featured job</span>
+            </div>
+            <div className='flex items-center mv-6 space-x-1'>
+                <BiCheck  className='w-7 h-7 '/>
+                <span className='text-gray-700 dark:text-gray-300'>Job displayed for 20 days</span>
+            </div>
+            <div className='flex items-center mv-6 space-x-1'>
+                <BiCheck  className='w-7 h-7 '/>
+                <span className='text-gray-700 dark:text-gray-300'>Premium Support 24/7</span>
+            </div>
+        </div>
+
+        {/* Button */}
+        <div className='mt-8 text-center'>
+            <button className='w-full py-3 bg-blue-900/15 dark:bg-gray-100 text-blue-700 rounded-full hover:bg-blue-700 hover:text-white transition-all duration-300 cursor-pointer'>View Profile</button>
+        </div>
     </div>
   )
 }
